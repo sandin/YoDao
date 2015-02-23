1fc6b46a618cf850631a54cf06ff91ab00160f11
lds <172339248@qq.com>
2015-02-23 15:14:36 +0800

    update testing

--- YodaoTest/src/com/yoda/yodao/test/App.java
+++ YodaoTest/src/com/yoda/yodao/test/App.java
@@ -1,22 +1,15 @@
 package com.yoda.yodao.test;
 
-import java.util.Arrays;
-
+import com.yoda.yodao.DaoFactory;
 import com.yoda.yodao.test.dao.UserDao;
-import com.yoda.yodao.test.dao.impl.UserDaoImpl;
 import com.yoda.yodao.test.model.User;
-import com.yodao.YoDaoFactory;
 
 public class App {
 
 	public static void main(String[] args) {
-		/*
-		 * UserDao userDao = YoDaoFactory.create(UserDao.class); User user =
-		 * userDao.findOne(1); user.getAge();
-		 */
-
-		String[] arr = new String[] { "a", "b" };
-		System.out.println(Arrays.toString(arr));
+		UserDao userDao = DaoFactory.create(UserDao.class);
+		User user = userDao.findOne(1);
+		user.getAge();
 	}
 
 }
